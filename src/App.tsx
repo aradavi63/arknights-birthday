05e9f0725222194596f536ecd1105322fad2370f
aradavi63 <adam.ahmadavi@gmail.com>
2025-06-14 21:57:48 +0800

    style buttons to match arknights ui style sort of

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 //import { useState } from 'react'
 import Calendar from './components/calendar'
 import Header from './components/header'
-import Sidebar from './components/sidebar'
 import './index.css'
 
 export default function App() {
   return (
     <>
     <Header />
-    <div className="grid grid-cols-2 gap-2">
-      <div>
-        <Sidebar />
-      </div>
-      <div col-span-full>
+    <div>
         <Calendar />
-      </div>
     </div>
     
     </>
