bb313f5c7bdfcd2b6dbe1eea9c511abcc1a01e41
aradavi63 <adam.ahmadavi@gmail.com>
2025-06-22 13:48:19 +0800

    add all operator birthdays to calendar (it works!)

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGrid from '@fullcalendar/daygrid'
 import multiMonth from '@fullcalendar/multimonth'
+import rrulePlugin from '@fullcalendar/rrule'
 //import interactionPlugin from "@fullcalendar/interaction"
 
 import '../styles/app.css'
@@ -11,52 +12,39 @@ import operatorData from '../operators.json'
 
 const unknownDob: string[] = [];
 
-function convertDateToTime(date: string): string{
-  const freq = 'FREQ=YEARLY;';
+function convertDateToTime(date: string): {freq: 'yearly', dtstart: string}  {
   let month = '';
-  const day = 'BYDAY=' + date.slice(-2);
-  if (date.startsWith('Jan')) {
-    month = 'BYMONTH=1;';
+  if (date.startsWith('Jan')) month = '01-';
+  else if (date.startsWith('Feb')) month = '02-';
+  else if (date.startsWith('Mar')) month = '03-';
+  else if (date.startsWith('Apr')) month = '04-';
+  else if (date.startsWith('May')) month = '05-';
+  else if (date.startsWith('Jun')) month = '06-';
+  else if (date.startsWith('Jul')) month = '07-';
+  else if (date.startsWith('Aug')) month = '08-';
+  else if (date.startsWith('Sep')) month = '09-';
+  else if (date.startsWith('Oct')) month = '10-';
+  else if (date.startsWith('Nov')) month = '11-';
+  else if (date.startsWith('Dec')) month = '12-';
+
+  const dayNum = date.slice(-2);
+  let day = '';
+  if (dayNum[0] === ' ') {
+    day = '0' + dayNum[1];
   }
-  else if (date.startsWith('Feb')) {
-    month = 'BYMONTH=2;';
+  else {
+    day = dayNum;
   }
-  else if (date.startsWith('Mar')) {
-    month = 'BYMONTH=3;';
-  }
-  else if (date.startsWith('Apr')) {
-    month = 'BYMONTH=4;';
-  }
-  else if (date.startsWith('May')) {
-    month = 'BYMONTH=5';
-  }
-  else if (date.startsWith('Jun')) {
-    month = 'BYMONTH=6;';
-  }
-  else if (date.startsWith('Jul')) {
-    month = 'BYMONTH=7;';
-  }
-  else if (date.startsWith('Aug')) {
-    month = 'BYMONTH=8;';
-  }
-  else if (date.startsWith('Sep')) {
-    month = 'BYMONTH=9;';
-  }
-  else if (date.startsWith('Oct')) {
-    month = 'BYMONTH=10;';
-  }
-  else if (date.startsWith('Nov')) {
-    month = 'BYMONTH=11;';
-  }
-  else if (date.startsWith('Dec')) {
-    month = 'BYMONTH=12;';
-  }
-  const rule = freq+month+day;
-  return rule;
+  const dtstart = '1970-' + month + day;
+
+  return {
+    freq: 'yearly',
+    dtstart: dtstart
+  };
 }
 
 function addBirthdays() {
-  const events: { id: string, title: string; rrule: string, allDay: boolean }[] = [];
+  const events: { id: string, title: string; rrule: {freq: 'yearly', dtstart: string}, allDay: boolean }[] = [];
   for (const operator of Object.values(operatorData)) {
     if (isNaN(Number(operator.dob[operator.dob.length - 1]))) { // If last element of DOB not a number, should be unknown
       unknownDob.push(operator.id)
@@ -81,7 +69,7 @@ export default function Calendar() {
   return (
     <div className='max-w-7xl mx-auto'>
       <FullCalendar
-      plugins={[ dayGrid, multiMonth ]}
+      plugins={[ dayGrid, multiMonth, rrulePlugin ]}
       initialView="dayGridMonth"
       headerToolbar={{
         left: 'prev,next today',
@@ -95,8 +83,9 @@ export default function Calendar() {
         week: 'Week',
         day: 'Day'
       }}
-      height={800}
-      initialEvents={bdayEvents}
+      validRange={{start:'1970-01-01'}}
+      height={600}
+      events={bdayEvents}
     />
     </div>
   )
